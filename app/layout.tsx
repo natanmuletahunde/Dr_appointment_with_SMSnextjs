fdc11241903d7bf118a69e066e92ea9a0cec297d
pivnati <natanmuleta911@gmail.com>
2025-08-28 14:11:36 -1200

    install the theme-provider from the shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
-import {Plus_Jakarta_Sans} from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
-  weight:['300','400','500','600','700'],
-    variable: "--font-sans",
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
 });
-
-
 
 export const metadata: Metadata = {
   title: "CarePulse",
@@ -24,9 +23,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
