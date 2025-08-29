0a57d3132454298b5db0cbb463edefdd39708467
pivnati <natanmuleta911@gmail.com>
2025-08-28 13:50:02 -1200

    create the shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-
+import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div>
        <h1 className="text-white text-3xl underline">Home</h1>
+       <Button>Click Me</Button>
     </div>
   )
 }
