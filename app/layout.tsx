a4eaa5f260e9feb640d9a3e989b757e9d44b6a72
pivnati <natanmuleta911@gmail.com>
2025-08-28 13:14:09 -1200

    install the clsx and talwind-merge

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {Plus_Jakarta_Sans} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
+  weight:['300','400','500','600','700'],
+    variable: "--font-sans",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
-  title: "carepulse",
-  description: "healthcare management system",
+  title: "CarePulse",
+  description: "A healthcare management system",
 };
 
 export default function RootLayout({
@@ -25,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={}  
       >
         {children}
       </body>
