ef8f17d31e4491c7416e0237bb3bf7337ae7f8c3
pivnati <natanmuleta911@gmail.com>
2025-08-29 05:35:43 -1200

    install the  react hook form

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+"use client"
 
+import { z } from "zod"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
 const PatientForm = () => {
   return (
     <div>
-        PatientForm  
+        PatientForm
     </div>
   )
 }
