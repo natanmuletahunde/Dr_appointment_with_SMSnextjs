21b904c155bda68e6787d2bee73dc84530792d5b
pivnati <natanmuleta911@gmail.com>
2025-08-31 09:26:37 -1200

    modfiy the all the patient form

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,16 +1,20 @@
-import { Query } from "node-appwrite"
-import { users } from "../appwrite.config"
+// import { ID, Query } from "node-appwrite";
+// // import { users } from "../appwrite.config";
 
-export const createUser = async (user:CreateUserParams)=>{
-  try {
-      
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error:any) {
-   if(error && error?.code===409){
-    const documents = await users.list([
-        Query.equal('email' , [user.email])
-    ])
-    return documents?.users[0]
-   }
-  }
-}
+// export const createUser = async (user: CreateUserParams) => {
+//   try {
+//     const newUser = await users.create(
+//       ID.unique(),
+//       user.email,
+//       user.phone,
+//       undefined,
+//       user.name
+//     );
+//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   } catch (error: any) {
+//     if (error && error?.code === 409) {
+//       const documents = await users.list([Query.equal("email", [user.email])]);
+//       return documents?.users[0];
+//     }
+//   }
+// };
