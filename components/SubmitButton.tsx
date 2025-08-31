3a4c4a785481246e1979f8ebdaf7e7aa220a7df6
pivnati <natanmuleta911@gmail.com>
2025-08-30 18:44:47 -1200

    create all the attribute of the patient in the appwrite

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -2,27 +2,33 @@ import React from 'react'
 import { Button } from './ui/button'
 import Image from 'next/image'
 
-interface ButtonProps{
-    isLoading:boolean,
-    className:string,
-    children:React.ReactNode
+interface ButtonProps {
+  isLoading: boolean
+  className?: string   // 🔹 make optional
+  children: React.ReactNode
 }
-const SubmitButton = ({isLoading,className,children}:ButtonProps) => {
-  return (
-    <Button  type='submit' disabled={isLoading} className={className ?? 'shad-primary-btn w-full'}>
-      {isLoading ? (
-        <div className='flex items-center gap-4'>
-            <Image
-                src='/assets/icons/loader.svg'
-                alt='loader'
-                width={24}
-                height={24}
-                className='animate-spin'
-            />
-            Loader ...
-        </div>
-      ):children}
 
+const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
+  return (
+    <Button
+      type="submit"
+      disabled={isLoading}
+      className={className ?? 'shad-primary-btn w-full'} // default applied if none
+    >
+      {isLoading ? (
+        <div className="flex items-center gap-4">
+          <Image
+            src="/assets/icons/loader.svg"
+            alt="loader"
+            width={24}
+            height={24}
+            className="animate-spin"
+          />
+          Loader ...
+        </div>
+      ) : (
+        children
+      )}
     </Button>
   )
 }
