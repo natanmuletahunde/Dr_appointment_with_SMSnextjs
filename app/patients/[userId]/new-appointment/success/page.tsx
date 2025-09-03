4704ca8912f14d70459dae905a9a6705e845dace
pivnati <natanmuleta911@gmail.com>
2025-09-03 01:36:46 -1200

    finish the success page

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { getAppointment } from '@/lib/actions/appointment.actions';
 import { Doctors } from '@/constants';
+import { formatDateTime } from "@/lib/utils";
+import { Button } from '@/components/ui/button';
 
 const Success = async ({params:{userId},searchParams}:SearchParamProps) => {
 const appointmentId= (searchParams?.appointmentId as string) || "";
@@ -21,6 +23,49 @@ const doctor = Doctors.find((doc)=>doc.name === appointment.primaryPhysician);
             className="h-10 w-fit"
           />
         </Link>
+        <section className="flex flex-col items-center">
+          <Image
+            src="/assets/gifs/success.gif"
+            height={300}
+            width={280}
+            alt="success"
+          />
+          <h2 className="header mb-6 max-w-[600px] text-center">
+            Your <span className="text-green-500">appointment request</span> has
+            been successfully submitted!
+          </h2>
+          <p>We&apos;ll be in touch shortly to confirm.</p>
+        </section>
+        <section className="request-details">
+          <p>Requested appointment details: </p>
+          <div className="flex items-center gap-3">
+            <Image
+              // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+              src={doctor?.image!}
+              alt="doctor"
+              width={100}
+              height={100}
+              className="size-6"
+            />
+            <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+          </div>
+          <div className="flex gap-2">
+            <Image
+              src="/assets/icons/calendar.svg"
+              height={24}
+              width={24}
+              alt="calendar"
+            />
+            <p> {formatDateTime(appointment.schedule).dateTime}</p>
+          </div>
+        </section>
+        <Button variant="outline" className="shad-primary-btn" asChild>
+          <Link href={`/patients/${userId}/new-appointment`}>
+            New Appointment
+          </Link>
+        </Button>
+
+        <p className="copyright">© 2024 CarePluse</p>
         </div>
 
 
