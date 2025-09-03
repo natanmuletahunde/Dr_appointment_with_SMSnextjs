717348085398597a2b3acd939dcc572eb5883c77
pivnati <natanmuleta911@gmail.com>
2025-09-03 02:58:19 -1200

    add the getRecentAppointmentList  in the appointment.action.ts

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,7 +1,8 @@
 "use server";
-import { ID, } from "node-appwrite";
+import { ID, Query, } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
 
 
 //  CREATE APPOINTMENT
@@ -33,3 +34,46 @@ export const getAppointment = async (appointmentId:string)=>{
     console.log(error)
     }
 }
+
+export const getRecentAppointmentList = async () => {
+    try {
+      const appointments = await databases.listDocuments(
+        DATABASE_ID!,
+        APPOINTMENT_COLLECTION_ID!,
+        [Query.orderDesc("$createdAt")]
+      );
+      const initialCounts = {
+        scheduledCount: 0,
+        pendingCount: 0,
+        cancelledCount: 0,
+      };
+
+      const counts = (appointments.documents as unknown as Appointment[]).reduce(
+        (acc, appointment) => {
+            if (appointment.status === "scheduled") {
+                acc.scheduledCount += 1;
+            } else if (appointment.status === "pending") {
+                acc.pendingCount += 1;
+            } else if (appointment.status === "cancelled") {
+                acc.cancelledCount += 1;
+          }
+          return acc;
+        },
+        initialCounts
+      );
+
+      const data = {
+        totalCount: appointments.total,
+        ...counts,
+        documents: appointments.documents,
+      };
+
+      return parseStringify(data);
+    } catch (error) {
+      console.error(
+        "An error occurred while retrieving the recent appointments:",
+        error
+      );
+    }
+  };
+
