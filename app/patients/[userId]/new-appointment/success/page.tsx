349ea8dd25442a7f996d93b146963cef6c2e3a55
pivnati <natanmuleta911@gmail.com>
2025-09-03 01:34:00 -1200

    create the doctors from the constant

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { getAppointment } from '@/lib/actions/appointment.actions';
+import { Doctors } from '@/constants';
 
-const Success = () => {
+const Success = async ({params:{userId},searchParams}:SearchParamProps) => {
+const appointmentId= (searchParams?.appointmentId as string) || "";
+const appointment = await getAppointment(appointmentId);
+const doctor = Doctors.find((doc)=>doc.name === appointment.primaryPhysician);
+
   return (
-    <div>
-    Success
+    <div className='flex h-screen max-h-screen px[5%]'>
+        <div className='success-img'>
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="h-10 w-fit"
+          />
+        </Link>
+        </div>
+
+
     </div>
   )
 }
