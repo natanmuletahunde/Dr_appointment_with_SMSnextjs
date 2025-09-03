a0caa3917d84ba3360be985a439448a2ff02ffff
pivnati <natanmuleta911@gmail.com>
2025-09-03 02:28:54 -1200

    install the input OTP shadcn ui

--- components/PassKeyModal.tsx
+++ components/PassKeyModal.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const PassKeyModal = () => {
   return (
-    <div>
-     PassKeyModal
-    </div>
-  )
-}
+    <AlertDialog>
+      <AlertDialogTrigger>Open</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
 
-export default PassKeyModal
+export default PassKeyModal;
